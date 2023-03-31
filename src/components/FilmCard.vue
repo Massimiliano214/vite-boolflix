8c3c0f2c8ce14ad00d574b0075846f3eca50ef09
Massimiliano Fanizza <fanizzamassimiliano@gmail.com>
2023-03-31 16:55:16 +0200

    added flaggs

--- src/components/FilmCard.vue
+++ src/components/FilmCard.vue
@@ -15,7 +15,10 @@
     <div>
         <h3> {{ title }}</h3>
         <h3> {{ originalTitle }}</h3>
-        <h3> {{ language }}</h3>
+        <h3 v-if="language == 'it'"> <img src="../../public/download_itas.png" alt="bandiera italiana"></h3>
+        <h3 v-else-if="language == 'en'"> <img src="../../public/download.png" alt="bandiera inglese"></h3>
+        <h3 v-else-if="language == 'pt'"> <img src="../../public/download_porto.png" alt="bandiera portoghese"></h3>
+        <h3 v-else> {{ language }}</h3>
         <h3> {{ score }}</h3>
     </div>
 
