b0a830e0b79ade9e359401634de1295d2601c1f0
Massimiliano Fanizza <fanizzamassimiliano@gmail.com>
2023-03-31 16:15:28 +0200

    research with api added

--- src/App.vue
+++ src/App.vue
@@ -14,9 +14,18 @@
     },
     methods: {
       getFilms() {
-        let urlApi = "`https://api.themoviedb.org/3/search/movie?api_key=4a8514f5e3a15bb52954d6f04549524a&query=ritorno+al+futuro&language=it-IT`";
+
+        let urlApi = `https://api.themoviedb.org/3/search/movie?api_key=4a8514f5e3a15bb52954d6f04549524a&query=${store.search.replace(" ","+")}&language=it-IT`;
 
         console.log(urlApi);
+
+        axios.get(urlApi)
+        .then(response => {
+          this.store.filmList = response.data.results;
+          console.log(this.store.filmList);
+        })
+        
+        
       }
     }
   }
--- src/store.js
+++ src/store.js
@@ -3,5 +3,7 @@ import { reactive } from "vue";
 export const store = reactive(
     {
         search: "",
+        filmList: [],
+        emits: ["updatedcount"],
     }
 );
