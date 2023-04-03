90028b4ce54edaf47f06c4125f0580d08ee22e25
Massimiliano Fanizza <fanizzamassimiliano@gmail.com>
2023-04-03 18:39:10 +0200

    added properties to tvseries component

--- src/components/TvSeriesCard.vue
+++ src/components/TvSeriesCard.vue
@@ -25,27 +25,42 @@
 
 
 <template>
+    <div class="filmCard" :style = "`background-image: url(${image})`">
+        <li class="tvSeriesCard">
+            <h3 v-if="title.toLowerCase() != originalTitle.toLowerCase()"> {{ title }}</h3>
+            <h3> {{ originalTitle }}</h3>
+            <span class="flag" v-if="language == 'it'"><span class="fi fi-it"></span></span>
+            <span class="flag" v-else-if="language == 'en'"><span class="fi fi-gb"></span></span>
+            <span class="flag" v-else-if="language == 'pt'"><span class="fi fi-pt"></span></span>
+            <span v-else> {{ language }}</span> <br>
+            <span v-for="(score, index) in scoreCoverter()" :key="index"><i class="fa-solid fa-star"></i></span>
+            <span v-for="(score, index) in 5 - scoreCoverter()" :key="index"><i class="fa-regular fa-star"></i></span>
+        </li>
+    </div>
     
-    <li class="tvSeriesCard">
-        <h3 v-if="title.toLowerCase() != originalTitle.toLowerCase()"> {{ title }}</h3>
-        <h3> {{ originalTitle }}</h3>
-        <h3 v-if="language == 'it'"> <img src="/download_itas.png" alt="bandiera italiana"></h3>
-        <h3 v-else-if="language == 'en'"> <img src="/download.png" alt="bandiera inglese"></h3>
-        <h3 v-else-if="language == 'pt'"> <img src="/download_porto.png" alt="bandiera portoghese"></h3>
-        <h3 v-else> {{ language }}</h3>
-        <span v-for="(score, index) in scoreCoverter()" :key="index"><i class="fa-solid fa-star"></i></span>
-        <span v-for="(score, index) in 5 - scoreCoverter()" :key="index"><i class="fa-regular fa-star"></i></span>
-        <img :src="image" alt="foto Serie Tv">
-    </li>
 
 </template>
 
 <style scoped lang="scss">
-    li {
+    @use "/node_modules/flag-icons/css/flag-icons.min.css";
+
+    .flag {
+        font-size: 2rem;
+        padding: 1.5rem;
+    }
+    .filmCard {
         
-        background-image: url(image);
-        
+        background-size: cover;
+        width: 100%;
+        height: 420px;
+        margin-bottom: 20px;
+        color: white;
+    
         list-style-type: none;
+
+        .liContainer {
+            display: none;
+        }
 
         img {
             width: 100%;
@@ -53,5 +68,13 @@
         * {
             padding: 5px;
         }
+    }
+
+    .filmCard:hover {
+        background-image: none;
+        background-color: black;
+       .liContainer {
+        display: block;
+       } 
     }
 </style>
